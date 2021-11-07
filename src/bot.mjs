31557709426998695e674e34f2e5e1f7a5fa2149
Id Brahim Hakim <la208531@student.helha.be>
2021-11-07 19:11:14 +0100

    ajusteemnt

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,83 +1,93 @@
-import { } from 'dotenv/config';
-import { Client, Intents } from 'discord.js';
-import { Gold, Hello } from './cmd_startwith.js';
-import { kick_id, ban_id } from './moderator_function.js';
-import { bio, status } from './statut.js';
-import { Consignes1, Consignes2, Consignes3 } from './consignes_function.js';
-import { partialMessage, roleAddJeux, roleRemoveJeux, msgAddReaction, msgRemoveReaction } from './function_roles.js';
+import {} from "dotenv/config";
+import { Client, Intents } from "discord.js";
+import { Gold, Hello } from "./cmd_startwith.js";
+import { kick_id, ban_id } from "./moderator_function.js";
+import { bio, status } from "./statut.js";
+import { Consignes1, Consignes2, Consignes3 } from "./consignes_function.js";
+import {
+  partialMessage,
+  roleAdd,
+  roleRemove,
+  msgAddReaction,
+  msgRemoveReaction
+} from "./function_roles.js";
+
+process.setMaxListeners(0);
 
 const client = new Client({
-	intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
-	partials: ['MESSAGE', 'CHANNEL', 'REACTION'],
+  intents: [
+    Intents.FLAGS.GUILDS,
+    Intents.FLAGS.GUILD_MESSAGES,
+    Intents.FLAGS.GUILD_MESSAGE_REACTIONS
+  ],
+  partials: ["MESSAGE", "CHANNEL", "REACTION"]
 });
-const PREFIX = "Jinx "
+const PREFIX = "Jinx ";
 
-client.on('ready', () => {
+client.on("ready", () => {
+  setInterval(() => {
+    var Description_Status = bio();
+    client.user.setActivity(Description_Status.message, {
+      type: Description_Status.type
+    });
+  }, 5000);
 
-    setInterval(() => {
-        var Description_Status = bio();
-        client.user.setActivity(Description_Status.message, { type: Description_Status.type })
-    }, 5000);
+  setInterval(() => {
+    var type_status = status();
+    client.user.setStatus(type_status);
+  }, 15000);
 
-    setInterval(() => {
-        var type_status = status();
-        client.user.setStatus(type_status)
-    }, 15000);
-
-    console.log(`le bot ${client.user.tag} est connecté`)
-
+  console.log(`le bot ${client.user.tag} est connecté`);
 });
 
-client.on('messageCreate', async (message) => {
-    if (message.author.bot) return;
+client.on("messageCreate", async message => {
+  if (message.author.bot) return;
 
-    const [CMD_NAME, ...args] = message.content
-        .trim()
-        .substring(PREFIX.length).
-        split(/\s+/);
+  const [CMD_NAME, ...args] = message.content
+    .trim()
+    .substring(PREFIX.length)
+    .split(/\s+/);
 
-    if (message.content.startsWith(PREFIX)) {
-        /** Fonction pour kick */
-        if (CMD_NAME === 'dégage-moi') {
-            kick_id(message, args);
-
-        } else if (CMD_NAME === 'ban-moi') {
-            ban_id(message, args)
-        }
-
-        /** Fonction pour afficher les consignes */
-        if (CMD_NAME === 'consignes') {
-            if (message.member.permissions.has('ADMINISTRATOR')) {
-                setTimeout(() => message.delete(), 1000);
-                Consignes1(message);
-                Consignes2(message);
-                Consignes3(message)
-            }
-        }
-
+  if (message.content.startsWith(PREFIX)) {
+    /** Fonction pour kick */
+    if (CMD_NAME === "dégage-moi") {
+      kick_id(message, args);
+    } else if (CMD_NAME === "ban-moi") {
+      ban_id(message, args);
     }
 
-    if (message.content.toLowerCase().includes('gold')) {
-        Gold(message)
+    /** Fonction pour afficher les consignes */
+    if (CMD_NAME === "consignes") {
+      if (message.member.permissions.has("ADMINISTRATOR")) {
+        setTimeout(() => message.delete(), 1000);
+        Consignes1(message);
+        Consignes2(message);
+        Consignes3(message);
+      }
     }
+  }
 
-    if (message.content.toLowerCase().includes('hello')) {
-        Hello(message)
-    }
+  if (message.content.toLowerCase().includes("gold")) {
+    Gold(message);
+  }
+
+  if (message.content.toLowerCase().includes("hello")) {
+    Hello(message);
+  }
 });
 
-client.on('messageReactionAdd', async (reaction, user) => {
-    partialMessage(reaction)
-    msgAddReaction(reaction)
+client.on("messageReactionAdd", async (reaction, user) => {
+  partialMessage(reaction);
+  msgAddReaction(reaction);
 
-    roleAddJeux(reaction, user)
+  roleAdd(reaction, user);
 });
 
-client.on('messageReactionRemove', async (reaction, user) => {
-    partialMessage(reaction)
-    msgRemoveReaction(reaction)
+client.on("messageReactionRemove", async (reaction, user) => {
+  partialMessage(reaction);
+  msgRemoveReaction(reaction);
 
-    roleRemoveJeux(reaction, user)
+  roleRemove(reaction, user);
 });
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
