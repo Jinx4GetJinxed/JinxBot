6168ebbca3ff4ff1f94038473c1c2e9ec0c7fe4d
Jinx4GetJinxed <swaggy6000@gmail.com>
2021-11-08 21:04:53 +0100

    ajout fonction youtube

--- src/bot.mjs
+++ src/bot.mjs
@@ -89,16 +89,20 @@ client.on("messageCreate", async message => {
         }
     }
 
-    const prefix2 = config.prefix
-    if (!message.content.startsWith(prefix2)) return;
-    const cmd = client.commands.get(CMD_NAME) || client.commands.get(client.aliases.get(CMD_NAME))
-    if (!cmd) return;
-    if (cmd.inVoiceChannel && !message.member.voice.channel) return message.channel.send(`${client.emotes.error} | Tu dois être dans un canal vocal chacal!`)
-    try {
-        cmd.run(client, message, args)
-    } catch (e) {
-        console.error(e)
-        message.reply(`Erreur: ${e}`)
+    if (message.content.startsWith(config.prefix)) {
+        const prefix2 = config.prefix
+        if (!message.content.startsWith(prefix2)) return;
+        const Args = message.content.slice(prefix2.length).trim().split(/ +/g)
+        const command = Args.shift().toLowerCase()
+        const cmd = client.commands.get(command) || client.commands.get(client.aliases.get(command))
+        if (!cmd) return console.log("pas bien");
+        if (cmd.inVoiceChannel && !message.member.voice.channel) return message.channel.send(`${client.emotes.error} | Tu dois être dans un canal vocal chacal!`)
+        try {
+            cmd.run(client, message, Args)
+        } catch (e) {
+            console.error(e)
+            message.reply(`Erreur: ${e}`)
+        }
     }
 
     if (message.content.toLowerCase().includes("gold")) {
@@ -123,5 +127,31 @@ client.on("messageReactionRemove", async (reaction, user) => {
 
     roleRemove(reaction, user);
 });
+
+const statut = queue => `Volume: \`${queue.volume}%\` | Filter: \`${queue.filters.join(", ") || "Off"}\` | Loop: \`${queue.repeatMode ? queue.repeatMode === 2 ? "All Queue" : "This Song" : "Off"}\` | Autoplay: \`${queue.autoplay ? "On" : "Off"}\``
+client.distube
+    .on("playSong", (queue, song) => queue.textChannel.send(
+        `${client.emotes.play} | Playing \`${song.name}\` - \`${song.formattedDuration}\`\nRequested by: ${song.user}\n${statut(queue)}`
+    ))
+    .on("addSong", (queue, song) => queue.textChannel.send(
+        `${client.emotes.success} | Added ${song.name} - \`${song.formattedDuration}\` to the queue by ${song.user}`
+    ))
+    .on("addList", (queue, playlist) => queue.textChannel.send(
+        `${client.emotes.success} | Added \`${playlist.name}\` playlist (${playlist.songs.length} songs) to queue\n${statut(queue)}`
+    ))
+    // DisTubeOptions.searchSongs = true
+    .on("searchResult", (message, result) => {
+        let i = 0
+        message.channel.send(`**Choose an option from below**\n${result.map(song => `**${++i}**. ${song.name} - \`${song.formattedDuration}\``).join("\n")}\n*Enter anything else or wait 60 seconds to cancel*`)
+    })
+    // DisTubeOptions.searchSongs = true
+    .on("searchCancel", message => message.channel.send(`${client.emotes.error} | Searching canceled`))
+    .on("error", (channel, e) => {
+        channel.send(`${client.emotes.error} | An error encountered: ${e}`)
+        console.error(e)
+    })
+    .on("empty", channel => channel.send("Voice channel is empty! Leaving the channel..."))
+    .on("searchNoResult", message => message.channel.send(`${client.emotes.error} | No result found!`))
+    .on("finish", queue => queue.textChannel.send("Finished!"))
 
 client.login(process.env.DISCORDJS_BOT_TOKEN);
