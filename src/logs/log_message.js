d480f692fe9623682dadbef0a8d80d9f2cdfcf0f
Hakim Id Brahim <la208531@student.helha.be>
2021-12-06 21:34:20 +0100

    golf

--- src/logs/log_message.js
+++ src/logs/log_message.js
@@ -8,13 +8,23 @@ function log_channel_message(client, num) {
   }
 }
 
-function log_message_delete(client, message) {
+async function log_message_delete(client, message) {
+  if (message.partial) {
+    try {
+      await message.fetch();
+    } catch (error) {
+      console.error("Une erreur est apparue à cause du fetch:", error);
+      return;
+    }
+  }
+
+  if (message.author.bot || !message.guild) return;
   const channel = log_channel_message(client, 1);
   return channel.send({
     embeds: [
       {
         author: {
-          name: message.author.user,
+          name: message.author.username,
           icon_url: message.author.avatarURL(),
         },
         color: "#9c2008",
